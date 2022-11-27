f869c25e7605e5f7dd2f38e2d413bcb28d197b53
Bryan Kleinberg <bmklei8p@gmail.com.here>
2022-11-27 11:26:31 -0500

    updated and installed dependencies

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import './api/server'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
